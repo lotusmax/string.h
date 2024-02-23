bc9bdc5ce7dfab9065a8201991aa89f53a57affd
lotusmax <157198610+lotusmax@users.noreply.github.com>
2024-02-23 12:36:17 +0900

    Add files via upload

--- s21_strcpsn.c
+++ s21_strcpsn.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef long unsigned s21_size_t;
 
-s21_size_t s21_strcspn(const char *str1, const char *str2) {
-    const char *p = str1;
-    const char *q;
-
-    while (*p) {
-        for (q = str2; *q && *q != *p; q++);
-        if (*q)
-            break;
-        p++;
+s21_size_t s21_strcpsn(const char *str1, const char *str2) {
+    s21_size_t symb = 0;
+    for(const char *a= str1; *a; a++) {
+        for(const char *b= str2; *b; b++) {
+            if (*a == *b) {
+                return symb;
+            }
+        }
+    symb++;
     }
-
-    return p - str1;
+    return symb;
 }
 
 int main() {
     const char *str1 = "bcdefa";
     const char *str2 = "a";
-    s21_size_t length = s21_strcspn(str1, str2);
+    s21_size_t length = s21_strcpsn(str1, str2);
     printf("Длина начального сегмента: %zu\n", length);
     return  0;
 } 
